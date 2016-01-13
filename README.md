d36838f5b0a0beeb216f79f395bb51772556f101
Micheal Parks <michealparks1989@gmail.com>
2016-01-13 16:58:39 -0500

    added support for browsers that don't support map.

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # micro-mediator
-A super tiny mediator for use with the publisher/subscribe pattern. Uses the [Map](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map) datatype. [Browser support](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map#Browser_compatibility) varies.
+A super tiny mediator for use with the publisher/subscribe pattern. Uses the [Map](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map) datatype. Reverts to plain objects in older browsers. 
+
+Use with browserify/webpack or just include and it'll install on `window`.
+
+Has the ability to use `subscribe` and `on` for subscription, `unsubscribe` and `off` for unsubscription, and `publish`, `emit`, and `trigger`.
 
 ```javascript
 var mediator = new require('micro-mediator')
@@ -16,7 +20,7 @@ mediator.off('party')
 Or install the mediator to objects of interest.
 
 ```javascript
-var o = {}, m = new Mediator()
+var o = {}, m = new window.Mediator()
 
 m.installTo(o)
 
@@ -24,5 +28,3 @@ o.on('imminent asteroid imapct', console.log)
 
 m.emit('imminent asteroid impact', 'panic')
 ```
-
-If in a non commonjs environment, the mediator will attach itself to ```window```.
--- micro-mediator.js
+++ micro-mediator.js
@@ -1,50 +1,55 @@
-(function(window, m) {
-
+(function (window, m) {
   if (typeof module !== 'undefined') module.exports = m
-  else window.Mediator = m 
+  else window.Mediator = m
+})(this, function () {
+  'use strict'
 
-})(this, function() {
+  try {
+    new window.Map()
+  } catch (e) {
+    window.Map = window.Object
+    window.Map.prototype.get = function (key) { return this[key] }
+    window.Map.prototype.set = function (key, val) { this[key] = val }
+    window.Map.prototype.has = function (key) { this[key] !== undefined }
+    window.Map.prototype.forEach = function (fn) {
+      window.Object.keys(this).forEach(function (key) { fn(this[key]) })
+    }
+  }
 
-'use strict'
+  window.console = window.console || { error: Function }
 
-console = console || { error: Function }
+  function Mediator () {
+    this.channels = new window.Map()
+    this.idProvider = 0
+  }
 
-function Mediator() {
-  this.channels = new Map()
-  this.idProvider = 0
+  Mediator.prototype.publish =
+  Mediator.prototype.emit =
+  Mediator.prototype.trigger = function (name, data) {
+    var channel = this.channels.get(name)
+    if (!channel) return console.error('Mediator: no such channel ' + channel)
+    channel.forEach(function (fn) { fn(data) })
+  }
 
-  if (!this.channels || !this.channels.forEach) 
-    console.error('Mediator: Map not supported by browser')
-}
+  Mediator.prototype.subscribe =
+  Mediator.prototype.on = function (name, fn) {
+    if (!this.channels.has(name)) this.channels.set(name, new Map())
+    this.channels.get(name).set(++this.idProvider, fn)
+    return this.idProvider
+  }
 
-Mediator.prototype.publish = 
-Mediator.prototype.emit =
-Mediator.prototype.trigger = function(name, data) {
-  var channel = this.channels.get(name)
-  if (!channel) return console.error('Mediator: no such channel ' + channel)
-  channel.forEach(function(fn) { fn(data) })
-}
+  Mediator.prototype.unsubscribe =
+  Mediator.prototype.off = function (name, id) {
+    var channel = this.channels.get(name)
+    if (!channel) return console.error('Mediator: no such channel ' + channel)
+    channel.delete(id)
+  }
 
-Mediator.prototype.subscribe = 
-Mediator.prototype.on = function(name, fn) {
-  if (!this.channels.has(name)) this.channels.set(name, new Map())
-  this.channels.get(name).set(++this.idProvider, fn)
-  return this.idProvider
-}
+  Mediator.prototype.installTo = function (obj) {
+    obj.publish = obj.emit = obj.trigger = this.publish.bind(this)
+    obj.subscribe = obj.on = this.subscribe.bind(this)
+    obj.unsubscribe = obj.off = this.unsubscribe.bind(this)
+  }
 
-Mediator.prototype.unsubscribe = 
-Mediator.prototype.off = function(name, id) {
-  var channel = this.channels.get(name)
-  if (!channel) return console.error('Mediator: no such channel ' + channel)
-  channel.delete(id)
-}
-
-Mediator.prototype.installTo = function(obj) {
-  obj.publish = obj.emit = obj.trigger = this.publish.bind(this)
-  obj.subscribe = obj.on = this.subscribe.bind(this)
-  obj.unsubscribe = obj.off = this.unsubscribe.bind(this)
-}
-
-return Mediator
-
+  return Mediator
 })
